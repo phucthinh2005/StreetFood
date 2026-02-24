2e83b20ec862a24ccf3b796ea5640cf6c14daec7
phucthinh2005 <thinhthy304@gmail.com>
2026-02-24 12:27:25 +0700

    Map

---
+++ MauiApp1/ViewModels/MapViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MauiApp1.ViewModels
+{
+    public class MapViewModel
+    {
+        public string Title { get; set; } = "Phố Ẩm Thực";
+    }
+}
